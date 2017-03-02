0055a01f0343664802dbab92fdb0db5646a1fb72
nguyenxuanluan <luannxse04649@fpt.edu.vn>
2017-03-02 13:05:23 +0700

    Tools

--- E.Z Lean/Screens/Tools/ToolViewController.swift
+++ E.Z Lean/Screens/Tools/ToolViewController.swift
@@ -7,8 +7,8 @@
 //
 
 import UIKit
-
-class ToolViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+import QuartzCore
+class ToolViewController: UIViewController{
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
@@ -16,52 +16,62 @@ class ToolViewController: UIViewController,UITableViewDelegate,UITableViewDataSo
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.dataSource = self
-        tableView.delegate = self
         button1.layer.cornerRadius = 5
         button1.layer.masksToBounds = true
         button2.layer.cornerRadius = 5
         button2.layer.masksToBounds = true
         button3.layer.cornerRadius = 5
         button3.layer.masksToBounds = true
-        
-        self.tableView.layer.borderColor = UIColor.white.cgColor
-        self.tableView.layer.borderWidth = 1;
-        self.tableView.layer.cornerRadius = 4;
-        
 
-        // Do any additional setup after loading the view.
     }
+    override func viewWillAppear(_ animated: Bool) {
+        setStartPosition()
+        DispatchQueue.main.async {
+            
+            self.button1.startAnimation(duration: 1,delay: 0.4, vector: CGVector(dx: 0, dy: 200), spring: 0.8)
+        }
+         DispatchQueue.main.async {
+            self.button2.startAnimation(duration: 1.5, delay: 0.2, vector: CGVector(dx: 0, dy: 325), spring: 0.7)
+        }
+         DispatchQueue.main.async {
+            self.button3.startAnimation(duration: 2, delay: 0.0, vector: CGVector(dx: 0, dy: 450), spring: 0.5)
+            
+        }
 
+    }
+    func setStartPosition(){
+        button1.frame.origin = CGPoint(x: view.frame.width/2-button1.frame.width/2, y: -button1.frame.height)
+        button2.frame.origin = CGPoint(x: view.frame.width/2-button1.frame.width/2, y: -button1.frame.height)
+        button3.frame.origin = CGPoint(x: view.frame.width/2-button1.frame.width/2, y: -button1.frame.height)
+        button1.alpha=0
+        button2.alpha=0
+        button3.alpha = 0
+    }
+    
+  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let label = cell.contentView.viewWithTag(101) as!  UILabel
-        print(indexPath.row)
-        if indexPath.row==0 {
-            label.text = "Tính toán chỉ số 1RM"
-        }
-        if indexPath.row==1 {
-            label.text = "Tính toán chỉ số Wilks"
-        }
-        if indexPath.row==2 {
-            label.text = "Tính toán chỉ số TDEE"
-        }
-        return cell
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //let touch = UITouch()
+            let location = touches.first?.location(in: self.button3)
+            print(location)
+            print(button3.point(inside: location!, with: nil))
+            if ((button3.layer.presentation()?.hitTest(location!)) != nil){
+                print("ABC")
+            }
+        
     }
     
-
+}
+extension UIButton{
+    func startAnimation(duration:Double,delay: Double? = 0.0,vector : CGVector,spring:Double){
+        let oldOrigin = self.frame.origin
+        UIView.animate(withDuration: duration, delay: delay!, usingSpringWithDamping: CGFloat(spring), initialSpringVelocity: 0, options: .curveEaseIn, animations: {
+            self.alpha = 1
+            self.frame.origin = oldOrigin.add(x: vector.dx, y: vector.dy)
+        }, completion: nil)
+    }
     
-
-    
-
 }
