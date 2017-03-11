0c33a9100e0d47d09f951463d24a262223983ffa
nguyenxuanluan <luannxse04649@fpt.edu.vn>
2017-03-12 00:07:49 +0700

    song

---
+++ E.Z Lean/Screens/Contact/ListSongTableViewCell.swift
@@ -0,0 +1,49 @@
+//
+//  ListSongTableViewCell.swift
+//  E.Z Lean
+//
+//  Created by LuanNX on 3/11/17.
+//  Copyright © 2017 E.Z Lean. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+import IBAnimatable
+class ListSongTableViewCell: UITableViewCell {
+    
+
+    @IBOutlet weak var playButton: AnimatableButton!
+    @IBOutlet weak var author: UILabel!
+    @IBOutlet weak var name: UILabel!
+    var songsArr = [Song]()
+    var source:String!
+    override func awakeFromNib() {
+       super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    func setUp(song: Song){
+        self.name.text = song.name
+        self.author.text = song.author
+        self.source = song.source
+    }
+    func loadData(api:String){
+        Alamofire.request(api).responseJSON(){
+            (response) in
+            if let result = response.result.value {
+                let temp = Song.parseToSong(json: JSON(result))
+                self.setUp(song: temp)
+                self.songsArr.append(temp)
+                print(self.songsArr.count)
+            }
+        }
+        
+    }
+
+}
